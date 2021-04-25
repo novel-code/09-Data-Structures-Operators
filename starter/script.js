d7daf0fc2ca756c4ea0c402a3b91e640ec8bbbb1
syed <novelcode0@gmail.com>
2021-04-25 22:43:42 +0530

    lec 119 coding challenge

--- starter/script.js
+++ starter/script.js
@@ -50,7 +50,7 @@ for (const [team, odd] of Object.entries(game.odds)) {
   console.log(`Odd of ${teamStr} ${odd}`);
 }
 */; ////////////////////////////////////////////////////////////////////////////// // Lec 115
-
+/*
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 const openingHours = {
@@ -98,7 +98,7 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-
+*/
 // Lec 113
 /*
 // Properties Names
@@ -371,36 +371,35 @@ console.log(p, q, r);
 */
 ////////////////////////////////////////////////////////////////////////////////
 
-/* CODING CHALLENGE (data structures, modern operators and strings)
-  Coding Challenge #1
+//  CODING CHALLENGE (data structures, modern operators and strings)
+//   Coding Challenge #1
 
-  We're building a football betting app (soccer for my American friends 😅)!
-Suppose we get data from a web service about a certain game ('game' variable on
-next page). In this challenge we're gonna work with that data.
-Your tasks:
-1. Create one player array for each team (variables 'players1' and
-'players2')
-2. The first player in any player array is the goalkeeper and the others are field
-players. For Bayern Munich (team 1) create one variable ('gk') with the
-goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-field players
-3. Create an array 'allPlayers' containing all players of both teams (22
-players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
-new array ('players1Final') containing all the original team1 players plus
-'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called
-'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player
-names (not an array) and prints each of them to the console, along with the
-number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which
-team is more likely to win, without using an if/else statement or the ternary
-operator.
-Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
-Then, call the function again with players from game.scored
-GOOD LUCK 😀
-*/
+//   We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+// GOOD LUCK 😀
 /*
 const game = {
   team1: 'Bayern Munich',
@@ -621,6 +620,7 @@ console.log(rest.size);
 console.log(rest.get(arr));
 */
 // MAPS: Iteration Lec 117
+/*
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
   [1, 'C'],
@@ -660,3 +660,71 @@ console.log([...question]);
 // console.log(question.entries());
 console.log([...question.values()]);
 console.log([...question.keys()]);
+*/
+// lec 119 coding challenge 3
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17:
+// ⚽
+// GOAL
+// GOOD LUCK 😀
+/*
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3 jonas method
+console.log(
+  `An event happend, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happend, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4 jonas method
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+
+// 4
+for (const [key, event] of gameEvents) {
+  if (key < 45) {
+    console.log(`[FIRST HALF]${key}: ${event}`);
+  } else if (key > 45) {
+    console.log(`[SECOND HALF]${key}: ${event}`);
+  }
+}
+*/
+// strings p-1 LEC 120
