d1e511ed6882e297c520e41f614f0910e335c872
syed <novelcode0@gmail.com>
2021-04-26 16:43:58 +0530

    lec 122 p3

--- starter/script.js
+++ starter/script.js
@@ -837,3 +837,52 @@ checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
 */
 // strings p-3 LEC 122
+// Split and Join
+console.log('a+very+nice+string'.split('+'));
+console.log('Syed Muzzammil'.split(' '));
+
+const [firstName, lastName] = 'Syed Muzzammil'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('syed mohammad muzzammil');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('syed'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(8847754));
+console.log(maskCreditCard(928843798998847754));
+console.log(maskCreditCard('83776459462964098'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... \n';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛬'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
